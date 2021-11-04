0b94c04f2d43e79c911b2963eafb7610b7097bbe
Влад Колобков <53657912+GreatSimbad@users.noreply.github.com>
2021-11-04 15:25:07 +0200

    Add files via upload

---
+++ src/js.js
@@ -0,0 +1,60 @@
+document.addEventListener('keydown', function(e) {
+    if (e.code == "KeyA") {
+        let audioA = new Audio("music/A.mp3").play();
+    }
+});
+document.addEventListener('keydown', function(e) {
+    if (e.code == "KeyS") {
+        let audioS = new Audio("music/S.mp3").play();
+    }
+});
+document.addEventListener('keydown', function(e) {
+    if (e.code == "KeyD") {
+        let audioD = new Audio("music/D.mp3").play();
+    }
+});
+document.addEventListener('keydown', function(e) {
+    if (e.code == "KeyF") {
+        let audioF = new Audio("music/F.mp3").play();
+    }
+});
+document.addEventListener('keydown', function(e) {
+    if (e.code == "KeyG") {
+        let audioG = new Audio("music/G.mp3").play();
+    }
+});
+document.addEventListener('keydown', function(e) {
+    if (e.code == "KeyH") {
+        let audioH = new Audio("music/H.mp3").play();
+    }
+});
+document.addEventListener('keydown', function(e) {
+    if (e.code == "KeyJ") {
+        let audioJ = new Audio("music/J.mp3").play();
+    }
+});
+document.addEventListener('keydown', function(e) {
+    if (e.code == "KeyW") {
+        let audioJ = new Audio("music/black_keys/W.mp3").play();
+    }
+});
+document.addEventListener('keydown', function(e) {
+    if (e.code == "KeyE") {
+        let audioJ = new Audio("music/black_keys/E.mp3").play();
+    }
+});
+document.addEventListener('keydown', function(e) {
+    if (e.code == "KeyT") {
+        let audioJ = new Audio("music/black_keys/T.mp3").play();
+    }
+});
+document.addEventListener('keydown', function(e) {
+    if (e.code == "KeyY") {
+        let audioJ = new Audio("music/black_keys/Y.mp3").play();
+    }
+});
+document.addEventListener('keydown', function(e) {
+    if (e.code == "KeyU") {
+        let audioJ = new Audio("music/black_keys/U.mp3").play();
+    }
+});
